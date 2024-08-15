d52d4631dc9950ec0bd75283e6fbc0f71f99f178
lomassubedi <subedilomas@gmail.com>
2024-08-15 20:48:22 +0545

    updated and added service.

--- README.md
+++ README.md
@@ -1,2 +1,31 @@
-# serial-log-capture
-Python script to capture serial log and store in a file
+# Serial Log Capture Utility Program
+
+Python script to capture serial log and log in a file
+
+### Enable and Start the Service
+
+1. Reload the systemd manager configuration:
+
+   ```bash
+   sudo systemctl daemon-reload
+   ```
+2. Enable the service to start on boot:
+
+   ```bash
+   sudo systemctl enable serial_log_capture.service
+   ```
+3. Start the service immediately:
+
+   ```bash
+   sudo systemctl start serial_log_capture.service
+   ```
+
+### Check the Status of the Service
+
+You can check if your service is running with:
+
+```bash
+sudo systemctl status serial_log_capture.service
+```
+
+This setup ensures that your Python script at /path/app.py will start on system boot and automatically restart if it crashes.
--- app.py
+++ app.py
@@ -24,8 +24,9 @@ def find_serial_port():
     raise RuntimeError("No suitable serial port found")
 
 
-async def read_serial_and_send_data(websocket, path):
+def read_serial_and_send_data():
     port = find_serial_port()
+    print("Using port : " + str(port))
     try:
         # Open serial port
         ser = serial.Serial(port, BAUD_RATE)
@@ -37,21 +38,22 @@ async def read_serial_and_send_data(websocket, path):
                 data = ser.readline().decode()
                 if data:
                     # Send data to WebSocket clients
-                    await websocket.send(data)
-                    print("Data sent to client:", data)  # Debug message
+                    # await websocket.send(data)
+                    print("Data saved to file:", data)  # Debug message
                     # Log data to file
                     log_file.write(data + '\n')
-                await asyncio.sleep(0.01)  # Adjust sleep time as needed
+                    time.sleep(0.01)  # Adjust sleep time as needed
     except Exception as e:
         print(f"Error reading from serial port: {e}. Restarting...")
-        await asyncio.sleep(1)
+        time.sleep(1)
 
 
-async def run_websocket_server():
-    async with websockets.serve(read_serial_and_send_data, "localhost", 8765):
-        print("WebSocket server started.")
-        await asyncio.Future()  # Keep server running indefinitely
+# async def run_websocket_server():
+#     async with websockets.serve(read_serial_and_send_data, "localhost", 8765):
+#         print("WebSocket server started.")
+#         await asyncio.Future()  # Keep server running indefinitely
 
 
 if __name__ == "__main__":
-    asyncio.run(run_websocket_server())
+    # asyncio.run(run_websocket_server())
+    read_serial_and_send_data()
