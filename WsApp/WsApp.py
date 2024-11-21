b2789c82fa77fc8cf0bdea9cff27e58832a812f1
Nabin-Flash320 <nabindangi32@gmail.com>
2024-11-21 16:57:59 +0545

    Flask app with service and websocekt to read and publish serially read data

--- WsApp/WsApp.py
+++ WsApp/WsApp.py
@@ -1,9 +1,28 @@
 from flask import Flask, render_template
 from flask_socketio import SocketIO
+import zmq
+import threading
+import sys
+import signal
+
+run_read_thread = True
+context = zmq.Context() 
+socket = context.socket(zmq.SUB) 
+socket.connect("tcp://localhost:5555") 
+socket.setsockopt_string(zmq.SUBSCRIBE, "") # Subscribe to all messages
 
 # Create Flask app and initialize SocketIO
 app = Flask(__name__)
 socketio = SocketIO(app)
+
+def zmq_listener(): 
+    while run_read_thread: 
+        try:
+            message = socket.recv_string() 
+            print(f"Received message: {message}") 
+            socketio.emit('serial_data', {'data': message}) # Emit message to websocket clients
+        except:
+            pass
 
 # Handle WebSocket connection event
 @socketio.on('connect')
@@ -25,5 +44,15 @@ def handle_disconnect():
 def index():
     return render_template("index.html")
 
+def signal_handler(sig, frame): 
+    global run_read_thread 
+    run_read_thread = False 
+    print('Shutting down...') 
+    sys.exit(0)
+
 if __name__ == '__main__':
+    signal.signal(signal.SIGINT, signal_handler) # Register the signal handler 
+    thread = threading.Thread(target=zmq_listener) 
+    thread.daemon = True 
+    thread.start()
     socketio.run(app, host='0.0.0.0', port=5000)
--- app.py
+++ app.py
@@ -1,15 +1,23 @@
 import serial
 import time
 from serial.tools.list_ports import comports
-import socketserver
-import threading
-import asyncio
-import websockets
+import zmq  
+import sys
+import signal
 
 # Define global variables
 BAUD_RATE = 115200
 LOG_FILE_PATH = "serial_data.txt"
 
+stop_reading = False
+
+def signal_handler(sig, frame):
+    global stop_reading
+    print('Caught SIGINT (Ctrl+C), exiting...')
+    # Perform any cleanup tasks before exiting
+    stop_reading = True
+
+signal.signal(signal.SIGINT, signal_handler)
 
 def find_serial_port():
     # Get a list of available serial ports
@@ -27,37 +35,37 @@ def find_serial_port():
 def read_serial_and_send_data():
     port = find_serial_port()
     print("Using port : " + str(port))
+
+    context = zmq.Context() 
+    socket = context.socket(zmq.PUB) # Using PUB for publishing messages 
+    socket.bind("tcp://localhost:5555") # Bind to a port
+
     try:
         # Open serial port
-        ser = serial.Serial(port, BAUD_RATE)
+        # ser = serial.Serial(port, BAUD_RATE)
 
         # Open log file in append mode
-        with open(LOG_FILE_PATH, 'a') as log_file:
-            while True:
+        with open(LOG_FILE_PATH, 'a') as log_file, serial.Serial(port, BAUD_RATE) as ser:
+            while not stop_reading:
                 # Read data from serial port
                 try:
                     data = ser.readline().decode()
                     if data:
                         # Send data to WebSocket clients
-                        # await websocket.send(data)
                         print("Data saved to file:", data)  # Debug message
                         # Log data to file
                         log_file.write(data + '\n')
+                        socket.send_string(data)
                 except:
                     pass
                 finally:
                     time.sleep(0.01)  # Adjust sleep time as needed
+            print("stop reading")
     except Exception as e:
         print(f"Error reading from serial port: {e}. Restarting...")
         time.sleep(1)
-
-
-# async def run_websocket_server():
-#     async with websockets.serve(read_serial_and_send_data, "localhost", 8765):
-#         print("WebSocket server started.")
-#         await asyncio.Future()  # Keep server running indefinitely
+    context.term()
 
 
 if __name__ == "__main__":
-    # asyncio.run(run_websocket_server())
     read_serial_and_send_data()
